6276d9c2848c7442230f20db82a2d01c73d6745b
@pilumus <vlad-sid96@yandex.ru>
2022-10-30 17:11:37 +0400

    small refinements for understanding

--- Scripts/djsite/polls/views.py
+++ Scripts/djsite/polls/views.py
@@ -38,6 +38,7 @@ def results(request, question_id):
     # return HttpResponse(response % question_id)
 
 def vote(request, question_id):
+    """From Tutorial 4"""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
