fa672da0bf0aef771c1d27996a37a32cd68c8ffc
@pilumus <vlad-sid96@yandex.ru>
2022-11-07 13:42:56 +0400

    add in Stock quantity to the Goods page.

--- Scripts/djsite/dfshop/views.py
+++ Scripts/djsite/dfshop/views.py
@@ -2,8 +2,10 @@ from .models import Good, Stock
 from django.shortcuts import render
 
 def goods(request):
-    goods_list = Good.objects.all()
-    context = {'goods_list': goods_list}
+    goods_list = Good.objects.all() #QuerySet
+    stock = Stock.objects.all()
+    context = {'goods_list': goods_list,
+               'stock': stock}
     return render(request, 'dfshop/goods.html', context)
 
 # Create your views here.
