c9b7b8e479a9e7143286553316807de9fcf9c9cb
@pilumus <vlad-sid96@yandex.ru>
2022-11-08 11:13:43 +0400

    make a path() to show good in url

--- Scripts/djsite/dfshop/urls.py
+++ Scripts/djsite/dfshop/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('goods/', views.goods, name='goods'),
+    path('goods/<str:good>/good_details', views.good_details, name='good_details')
 ]
--- Scripts/djsite/dfshop/views.py
+++ Scripts/djsite/dfshop/views.py
@@ -1,5 +1,6 @@
 from .models import Good, Stock
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def goods(request):
     goods_list = Good.objects.all() #QuerySet
@@ -7,5 +8,8 @@ def goods(request):
     context = {'goods_list': goods_list,
                'stock': stock}
     return render(request, 'dfshop/goods.html', context)
+
+def good_details(request, good):
+    return HttpResponse("You're looking at %s." % good)
 
 # Create your views here.
